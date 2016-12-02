b85ea289c5b1851c725e02aa74cdbc99e1c80ec5
CrimsonGlory <javierbassi@gmail.com>
2016-12-02 19:27:52 -0300

    forgot to push this file -_-

---
+++ src/Api/queue_count.py
@@ -0,0 +1,15 @@
+from bottle import route
+from bottle import request
+from bottle import response
+import os
+path = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '..'))
+import sys
+sys.path.insert(0, path)
+from bson.json_util import dumps
+from Utils.Functions import number_of_jobs_on_queue
+
+@route('/api/v1/queue_count', method='GET')
+def task_finished():
+    count = number_of_jobs_on_queue('task')
+    return dumps({"count": count})
